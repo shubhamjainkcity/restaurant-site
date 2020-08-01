d3c53c087ed731f2bc38cc5f045469e2d96d6663
shubhamjainkcity <62244970+shubhamjainkcity@users.noreply.github.com>
2020-08-01 14:36:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { render } from '@testing-library/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+import Main from "./components/MainComponent";
+import "./App.css";
+
+const store = ConfigureStore();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
   render() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
-    </div>
-  );
-}
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
